22f4a64d4c155fdbce4c59edd33717bb74f6a89e
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-26 16:14:11 +0100

    🚧 generate sub-changelogs in sync mode: default current version to 0.0.0
    #42

--- packages/semver/src/builders/version/utils/get-current-version.spec.ts
+++ packages/semver/src/builders/version/utils/get-current-version.spec.ts
@@ -1,18 +1,28 @@
 import { Observable, of } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { readPackageJson } from './project';
+import { hasPackageJson, readPackageJson } from './project';
 import * as projectUtils from './project';
 
+jest.mock('./project');
+
 function getCurrentVersion(projectRoot: string): Observable<string> {
-  return readPackageJson(projectRoot).pipe(
-    map((packageInfo) => packageInfo.version as string)
-  );
+  if (hasPackageJson(projectRoot)) {
+    return readPackageJson(projectRoot).pipe(
+      map((packageInfo) => packageInfo.version as string)
+    );
+  }
+
+  return of('0.0.0');
 }
 
 describe('getCurrentVersion', () => {
-  afterEach(() => (readPackageJson as jest.Mock).mockRestore());
+  afterEach(() => {
+    (hasPackageJson as jest.Mock).mockRestore();
+    (readPackageJson as jest.Mock).mockRestore();
+  });
 
   it('should compute current version from package.json', async () => {
+    jest.spyOn(projectUtils, 'hasPackageJson').mockReturnValue(true);
     jest.spyOn(projectUtils, 'readPackageJson').mockReturnValue(
       of({
         version: '2.1.0',
@@ -23,9 +33,15 @@ describe('getCurrentVersion', () => {
     expect(readPackageJson).toBeCalledWith('/root');
   });
 
+  /* Cf. https://github.com/conventional-changelog/standard-version/blob/master/lib/latest-semver-tag.js */
   it.todo(
     '🚧 should compute current version from tags if package.json is not present'
   );
 
-  it.todo('🚧 should default to 0.0.0');
+  it('should default to 0.0.0', async () => {
+    jest.spyOn(projectUtils, 'hasPackageJson').mockReturnValue(false);
+    const version = await getCurrentVersion('/root').toPromise();
+    expect(version).toEqual('0.0.0');
+    expect(hasPackageJson).toBeCalledWith('/root');
+  });
 });
