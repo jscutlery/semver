c7f62d198ae51dec06c81a624f3f6cc0a581ff06
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-26 15:52:47 +0100

    🚧 generate sub-changelogs in sync mode: get current version
    #42

--- packages/semver/src/builders/version/utils/get-current-version.spec.ts
+++ packages/semver/src/builders/version/utils/get-current-version.spec.ts
@@ -1,5 +1,35 @@
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { readPackageJson } from './project';
+import * as projectUtils from './project';
+
+function getCurrentVersion({
+  projectRoot,
+}: {
+  projectRoot: string;
+}): Observable<string> {
+  return readPackageJson({ projectRoot }).pipe(
+    map((packageInfo) => packageInfo.version as string)
+  );
+}
+
 describe('getCurrentVersion', () => {
-  it.todo('🚧 should compute current version from package.json');
+  afterEach(() => (readPackageJson as jest.Mock).mockRestore());
+
+  it('should compute current version from package.json', async () => {
+    jest.spyOn(projectUtils, 'readPackageJson').mockReturnValue(
+      of({
+        version: '2.1.0',
+      })
+    );
+    const version = await getCurrentVersion({
+      projectRoot: '/root',
+    }).toPromise();
+    expect(version).toEqual('2.1.0');
+    expect(readPackageJson).toBeCalledWith({
+      projectRoot: '/root',
+    });
+  });
 
   it.todo(
     '🚧 should compute current version from tags if package.json is not present'
---
+++ packages/semver/src/builders/version/utils/project.ts
@@ -0,0 +1,6 @@
+import { JsonObject } from '@angular-devkit/core';
+import { Observable } from 'rxjs';
+
+export function readPackageJson({ projectRoot }): Observable<JsonObject> {
+  throw new Error('🚧 work in progress!');
+}
