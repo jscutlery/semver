a8490992bff73e0dd960247f692bf778dc233c2a
Younes Jaaidi <yjaaidi@gmail.com>
2021-01-26 22:28:16 +0100

    🚧 generate sub-changelogs in sync mode: add getCommits
    #42

--- packages/semver/src/builders/version/utils/git.ts
+++ packages/semver/src/builders/version/utils/git.ts
@@ -1,11 +1,30 @@
+import * as gitRawCommits from 'git-raw-commits';
+
 import { Observable } from 'rxjs';
+import { last, scan, startWith } from 'rxjs/operators';
 
 /**
- * @deprecated 🚧 Work in progress.
+ * Return the list of commits since `since` commit.
  */
-export function getCommits(args: {
+export function getCommits({
+  projectRoot,
+  since,
+}: {
   projectRoot: string;
   since: string;
 }): Observable<string[]> {
-  throw new Error('🚧 work in progress!');
+  return new Observable<string>((observer) => {
+    gitRawCommits({
+      from: since,
+      path: projectRoot,
+    })
+      .on('data', (data: string) => observer.next(data))
+      .on('error', (error) => observer.error(error))
+      .on('close', () => observer.complete())
+      .on('finish', () => observer.complete());
+  }).pipe(
+    scan((commits, commit) => [...commits, commit], [] as string[]),
+    startWith([]),
+    last()
+  );
 }
