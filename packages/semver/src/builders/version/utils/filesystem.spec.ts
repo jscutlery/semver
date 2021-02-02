49d3789a1961a590c194229938676f4d8e576929
Younes Jaaidi <yjaaidi@gmail.com>
2021-02-02 15:40:10 +0100

    fix: 🐞 don't crash when `workspace.json` is not found
    closes #64

---
+++ packages/semver/src/builders/version/utils/filesystem.spec.ts
@@ -0,0 +1,35 @@
+import * as fs from 'fs';
+import { callbackify } from 'util';
+import { readJsonFile } from './filesystem';
+
+jest.mock('fs');
+
+describe('readJsonFile', () => {
+  let mockReadFile: jest.Mock;
+
+  beforeEach(() => {
+    mockReadFile = jest.fn();
+    jest
+      .spyOn(fs, 'readFile')
+      /* eslint-disable-next-line @typescript-eslint/no-explicit-any */
+      .mockImplementation(callbackify(mockReadFile) as any);
+  });
+
+  it('should emit error', async () => {
+    mockReadFile.mockRejectedValue(
+      new Error('ENOENT: no such file or directory')
+    );
+
+    const file$ = readJsonFile('/unexisting-file');
+
+    /* Wait for all microtasks to finish. */
+    /* We want to make sure that `fs.readFile` is not called
+     * before we subscribe, otherwise the error is not handled. */
+    await new Promise(setImmediate);
+
+    expect(file$.toPromise()).rejects.toThrow(
+      'ENOENT: no such file or directory'
+    );
+    expect(mockReadFile).toBeCalledTimes(1);
+  });
+});
--- packages/semver/src/builders/version/utils/filesystem.ts
+++ packages/semver/src/builders/version/utils/filesystem.ts
@@ -1,10 +1,10 @@
 import { readFile } from 'fs';
-import { from } from 'rxjs';
+import { defer } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { promisify } from 'util';
 
 export function readJsonFile(filePath: string) {
-  return from(promisify(readFile)(filePath, 'utf-8')).pipe(
+  return defer(() => promisify(readFile)(filePath, 'utf-8')).pipe(
     map((data) => JSON.parse(data))
   );
 }
