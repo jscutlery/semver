9593a371d757a7c13fc83ffa7f17336f260891c9
Gabor Hornyak <gabhornyak@gmail.com>
2021-03-04 21:42:22 +0100

    Cleanup

--- packages/semver/src/builders/version/utils/changelog.ts
+++ packages/semver/src/builders/version/utils/changelog.ts
@@ -1,7 +1,5 @@
-import { execFile } from 'child_process';
 import { resolve } from 'path';
 import { defer } from 'rxjs';
-import { promisify } from 'util';
 import * as changelog from 'standard-version/lib/lifecycles/changelog';
 import * as standardVersionDefaults from 'standard-version/defaults';
 
