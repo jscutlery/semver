b97448731b24e3278f77fa7576d25722280d2782
Edouard Bozon <bozonedouard@gmail.com>
2021-03-12 19:42:29 +0100

    🚧 enhance get current version

--- packages/semver/src/builders/version/utils/git.spec.ts
+++ packages/semver/src/builders/version/utils/git.spec.ts
@@ -3,193 +3,199 @@ import { of, throwError } from 'rxjs';
 import { PassThrough } from 'stream';
 
 import * as cp from './exec-async';
-import { getCommits, getFirstCommitRef, getLastTag, tryPushToGitRemote } from './git';
+import {
+  getCommits,
+  getFirstCommitRef,
+  getLastTag,
+  tryPushToGitRemote,
+} from './git';
 
 jest.mock('git-raw-commits', () => jest.fn());
 jest.mock('./exec-async');
 
-describe('git.getCommits', () => {
-  const mockGitRawCommits = gitRawCommits as jest.Mock;
+describe('git', () => {
+  afterEach(() => (cp.execAsync as jest.Mock).mockReset());
 
-  afterEach(() => (cp.execAsync as jest.Mock).mockRestore());
+  describe('getCommits', () => {
+    const mockGitRawCommits = gitRawCommits as jest.Mock;
 
-  it('should get commits list', () => {
-    const stream = new PassThrough();
-    mockGitRawCommits.mockReturnValue(stream);
+    it('should get commits list', () => {
+      const stream = new PassThrough();
+      mockGitRawCommits.mockReturnValue(stream);
 
-    const observer = {
-      next: jest.fn(),
-      complete: jest.fn(),
-    };
+      const observer = {
+        next: jest.fn(),
+        complete: jest.fn(),
+      };
 
-    getCommits({
-      projectRoot: 'libs/demo',
-      since: 'x1.0.0',
-    }).subscribe(observer);
+      getCommits({
+        projectRoot: 'libs/demo',
+        since: 'x1.0.0',
+      }).subscribe(observer);
 
-    stream.emit('data', 'feat A');
-    stream.emit('data', 'feat B');
-    stream.emit('close');
+      stream.emit('data', 'feat A');
+      stream.emit('data', 'feat B');
+      stream.emit('close');
 
-    expect(observer.next).toBeCalledTimes(1);
-    expect(observer.next).toBeCalledWith(['feat A', 'feat B']);
-    expect(observer.complete).toBeCalledTimes(1);
-  });
-
-  describe('git.tryPushToGitRemote', () => {
-    it('should Git push with right options', async () => {
-      jest
-        .spyOn(cp, 'execAsync')
-        .mockReturnValue(of({ stderr: '', stdout: 'success' }));
-
-      await tryPushToGitRemote({
-        remote: 'upstream',
-        branch: 'master',
-        noVerify: false,
-      }).toPromise();
-
-      expect(cp.execAsync).toBeCalledWith(
-        'git',
-        expect.arrayContaining([
-          'push',
-          '--follow-tags',
-          '--atomic',
-          'upstream',
-          'master',
-        ])
-      );
+      expect(observer.next).toBeCalledTimes(1);
+      expect(observer.next).toBeCalledWith(['feat A', 'feat B']);
+      expect(observer.complete).toBeCalledTimes(1);
     });
 
-    it(`should Git push and add '--no-verify' option when asked for`, async () => {
-      jest
-        .spyOn(cp, 'execAsync')
-        .mockReturnValue(of({ stderr: '', stdout: 'success' }));
+    describe('tryPushToGitRemote', () => {
+      it('should Git push with right options', async () => {
+        jest
+          .spyOn(cp, 'execAsync')
+          .mockReturnValue(of({ stderr: '', stdout: 'success' }));
 
-      await tryPushToGitRemote({
-        remote: 'origin',
-        branch: 'main',
-        noVerify: true,
-      }).toPromise();
+        await tryPushToGitRemote({
+          remote: 'upstream',
+          branch: 'master',
+          noVerify: false,
+        }).toPromise();
 
-      expect(cp.execAsync).toBeCalledWith(
-        'git',
-        expect.arrayContaining([
-          'push',
-          '--follow-tags',
-          '--no-verify',
-          '--atomic',
-          'origin',
-          'main',
-        ])
-      );
-    });
+        expect(cp.execAsync).toBeCalledWith(
+          'git',
+          expect.arrayContaining([
+            'push',
+            '--follow-tags',
+            '--atomic',
+            'upstream',
+            'master',
+          ])
+        );
+      });
 
-    it(`should retry Git push if '--atomic' option not supported`, async () => {
-      jest
-        .spyOn(cp, 'execAsync')
-        .mockReturnValueOnce(
-          throwError({ stderr: 'atomic failed', stdout: '' })
-        )
-        .mockReturnValueOnce(of({ stderr: '', stdout: 'success' }));
+      it(`should Git push and add '--no-verify' option when asked for`, async () => {
+        jest
+          .spyOn(cp, 'execAsync')
+          .mockReturnValue(of({ stderr: '', stdout: 'success' }));
 
-      jest.spyOn(console, 'warn').mockImplementation();
+        await tryPushToGitRemote({
+          remote: 'origin',
+          branch: 'main',
+          noVerify: true,
+        }).toPromise();
 
-      await tryPushToGitRemote({
-        remote: 'origin',
-        branch: 'master',
-        noVerify: false,
-      }).toPromise();
+        expect(cp.execAsync).toBeCalledWith(
+          'git',
+          expect.arrayContaining([
+            'push',
+            '--follow-tags',
+            '--no-verify',
+            '--atomic',
+            'origin',
+            'main',
+          ])
+        );
+      });
 
-      expect(cp.execAsync).toHaveBeenNthCalledWith(
-        1,
-        'git',
-        expect.arrayContaining(['push', '--atomic', '--follow-tags'])
-      );
-      expect(cp.execAsync).toHaveBeenNthCalledWith(
-        2,
-        'git',
-        expect.not.arrayContaining(['--atomic'])
-      );
-      expect(console.warn).toBeCalled();
-    });
+      it(`should retry Git push if '--atomic' option not supported`, async () => {
+        jest
+          .spyOn(cp, 'execAsync')
+          .mockReturnValueOnce(
+            throwError({ stderr: 'atomic failed', stdout: '' })
+          )
+          .mockReturnValueOnce(of({ stderr: '', stdout: 'success' }));
 
-    it(`should throw if Git push failed`, async () => {
-      jest
-        .spyOn(cp, 'execAsync')
-        .mockReturnValue(throwError({ stderr: 'failed', stdout: '' }));
+        jest.spyOn(console, 'warn').mockImplementation();
 
-      try {
         await tryPushToGitRemote({
           remote: 'origin',
           branch: 'master',
           noVerify: false,
         }).toPromise();
-        fail();
-      } catch (error) {
-        expect(cp.execAsync).toBeCalledTimes(1);
-        expect(error).toEqual(
-          expect.objectContaining({ stderr: 'failed', stdout: '' })
+
+        expect(cp.execAsync).toHaveBeenNthCalledWith(
+          1,
+          'git',
+          expect.arrayContaining(['push', '--atomic', '--follow-tags'])
         );
-      }
+        expect(cp.execAsync).toHaveBeenNthCalledWith(
+          2,
+          'git',
+          expect.not.arrayContaining(['--atomic'])
+        );
+        expect(console.warn).toBeCalled();
+      });
+
+      it(`should throw if Git push failed`, async () => {
+        jest
+          .spyOn(cp, 'execAsync')
+          .mockReturnValue(throwError({ stderr: 'failed', stdout: '' }));
+
+        try {
+          await tryPushToGitRemote({
+            remote: 'origin',
+            branch: 'master',
+            noVerify: false,
+          }).toPromise();
+          fail();
+        } catch (error) {
+          expect(cp.execAsync).toBeCalledTimes(1);
+          expect(error).toEqual(
+            expect.objectContaining({ stderr: 'failed', stdout: '' })
+          );
+        }
+      });
+
+      it('should fail if options are undefined', async () => {
+        try {
+          await tryPushToGitRemote({
+            remote: undefined as any,
+            branch: undefined as any,
+            noVerify: false,
+          }).toPromise();
+          fail();
+        } catch (error) {
+          expect(error.message).toContain('Missing Git options');
+        }
+      });
+    });
+  });
+
+  describe('getLastTag', () => {
+    it('should get last git commit', async () => {
+      jest
+        .spyOn(cp, 'execAsync')
+        .mockReturnValue(of({ stderr: '', stdout: '0.0.0' }));
+
+      const tag = await getLastTag().toPromise();
+
+      expect(tag).toBe('0.0.0');
+      expect(cp.execAsync).toBeCalledWith(
+        'git',
+        expect.arrayContaining(['describe', '--tags', '--abbrev=0'])
+      );
     });
 
-    it('should fail if options are undefined', async () => {
+    it('should throw on failure', async () => {
+      jest
+        .spyOn(cp, 'execAsync')
+        .mockReturnValue(of({ stderr: 'error', stdout: '' }));
+
       try {
-        await tryPushToGitRemote({
-          remote: undefined as any,
-          branch: undefined as any,
-          noVerify: false,
-        }).toPromise();
+        await getLastTag().toPromise();
         fail();
       } catch (error) {
-        expect(error.message).toContain('Missing Git options');
+        expect(error.message).toBe('No tag found');
       }
     });
   });
-});
 
-describe('git.getLastTag', () => {
-  it('should get last git commit', async () => {
-    jest
-      .spyOn(cp, 'execAsync')
-      .mockReturnValue(of({ stderr: '', stdout: '0.0.0' }));
+  describe('getFirstCommitRef', () => {
+    it('should get last git commit', async () => {
+      jest
+        .spyOn(cp, 'execAsync')
+        .mockReturnValue(of({ stderr: '', stdout: 'sha1\n' }));
 
-    const tag = await getLastTag().toPromise();
+      const tag = await getFirstCommitRef().toPromise();
 
-    expect(tag).toBe('0.0.0');
-    expect(cp.execAsync).toBeCalledWith(
-      'git',
-      expect.arrayContaining(['describe', '--tags', '--abbrev=0'])
-    );
-  });
-
-  it('should throw on failure', async () => {
-    jest
-      .spyOn(cp, 'execAsync')
-      .mockReturnValue(of({ stderr: 'error', stdout: '' }));
-
-    try {
-      await getLastTag().toPromise();
-      fail();
-    } catch (error) {
-      expect(error.message).toBe('No tag found');
-    }
+      expect(tag).toBe('sha1');
+      expect(cp.execAsync).toBeCalledWith(
+        'git',
+        expect.arrayContaining(['rev-list', '--max-parents=0', 'HEAD'])
+      );
+    });
   });
 });
-
-describe('git.getFirstCommitRef', () => {
-  it('should get last git commit', async () => {
-    jest
-      .spyOn(cp, 'execAsync')
-      .mockReturnValue(of({ stderr: '', stdout: 'sha1\n' }));
-
-    const tag = await getFirstCommitRef().toPromise();
-
-    expect(tag).toBe('sha1');
-    expect(cp.execAsync).toBeCalledWith(
-      'git',
-      expect.arrayContaining(['describe', '--tags', '--abbrev=0'])
-    );
-  });
-});
-
