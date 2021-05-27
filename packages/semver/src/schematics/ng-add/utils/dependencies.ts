af02a4c316e6a178dec8024f6350d9510111426d
RicardoJBarrios <ricardojbarrios@gmail.com>
2021-05-27 22:03:08 +0100

    feat(ng-add): add commitlint

--- packages/semver/src/schematics/ng-add/utils/dependencies.ts
+++ packages/semver/src/schematics/ng-add/utils/dependencies.ts
@@ -2,12 +2,29 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 import { SchemaOptions } from '../schema';
-import { getMergedPackageJsonConfig, getPackageJson, overwritePackageJson, PackageJson } from './package-json';
+import {
+  getMergedPackageJsonConfig,
+  getPackageJson,
+  overwritePackageJson,
+  PackageJson,
+  PackageJsonConfigPart,
+} from './package-json';
+
+const COMMITIZEN = {
+  commitizen: '^4.2.4',
+  'cz-conventional-changelog': '^3.3.0',
+};
+
+const COMMITLINT = {
+  '@commitlint/cli': '^12.1.4',
+  '@commitlint/config-conventional': '^12.1.4',
+};
 
 export function addDependencies(options: SchemaOptions): Rule {
   return (tree: Tree, context: SchematicContext) => {
     if (options.enforceConventionalCommits) {
       addCommitizen(options, tree, context);
+      addCommitlint(options, tree, context);
     }
   };
 }
@@ -17,30 +34,9 @@ export function addCommitizen(
   tree: Tree,
   context: SchematicContext
 ) {
-  addCommitizenDeps(options, tree, context);
+  addDevDependencies(COMMITIZEN, options, tree, context);
   addCommitizenConfig(tree);
   addCommitizenScrip(tree);
-}
-
-export function addCommitizenDeps(
-  options: SchemaOptions,
-  tree: Tree,
-  context: SchematicContext
-) {
-  const packageJson: PackageJson = getPackageJson(tree);
-  const deps = {
-    commitizen: '^4.2.4',
-    'cz-conventional-changelog': '^3.3.0',
-  };
-  const devDependencies = {
-    devDependencies: { ...packageJson.devDependencies, ...deps },
-  };
-  const newJson: PackageJson = getMergedPackageJsonConfig(
-    devDependencies,
-    packageJson
-  ) as PackageJson;
-  overwritePackageJson(tree, newJson);
-  installDependencies(options, context);
 }
 
 export function addCommitizenConfig(tree: Tree) {
@@ -74,6 +70,56 @@ export function addCommitizenScrip(tree: Tree) {
     packageJson
   ) as PackageJson;
   overwritePackageJson(tree, newJson);
+}
+
+export function addCommitlint(
+  options: SchemaOptions,
+  tree: Tree,
+  context: SchematicContext
+) {
+  addDevDependencies(COMMITLINT, options, tree, context);
+  addCommitlintConfig(tree);
+}
+
+export function addCommitlintConfig(tree: Tree) {
+  const packageJson: PackageJson = getPackageJson(tree);
+  const hasConfigFile =
+    tree.exists('commitlint.config.js') ||
+    tree.exists('commitlint') ||
+    tree.exists('.commitlintrc.js') ||
+    tree.exists('.commitlintrc.json') ||
+    tree.exists('.commitlintrc.yml');
+
+  if (!packageJson?.commitlint && !hasConfigFile) {
+    const config = {
+      commitlint: {
+        extends: ['@commitlint/config-conventional'],
+      },
+    };
+    const newJson: PackageJson = getMergedPackageJsonConfig(
+      config,
+      packageJson
+    ) as PackageJson;
+    overwritePackageJson(tree, newJson);
+  }
+}
+
+export function addDevDependencies(
+  deps: PackageJsonConfigPart<string>,
+  options: SchemaOptions,
+  tree: Tree,
+  context: SchematicContext
+) {
+  const packageJson: PackageJson = getPackageJson(tree);
+  const devDependencies = {
+    devDependencies: { ...packageJson.devDependencies, ...deps },
+  };
+  const newJson: PackageJson = getMergedPackageJsonConfig(
+    devDependencies,
+    packageJson
+  ) as PackageJson;
+  overwritePackageJson(tree, newJson);
+  installDependencies(options, context);
 }
 
 function installDependencies(
