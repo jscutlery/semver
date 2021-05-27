4a372748135ace7d7a02a90881197c741da304fc
RicardoJBarrios <ricardojbarrios@gmail.com>
2021-05-27 21:35:41 +0100

    feat(ng-add): add commitizen

--- packages/semver/src/schematics/ng-add/utils/dependencies.ts
+++ packages/semver/src/schematics/ng-add/utils/dependencies.ts
@@ -1,11 +1,87 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 import { SchemaOptions } from '../schema';
+import { getMergedPackageJsonConfig, getPackageJson, overwritePackageJson, PackageJson } from './package-json';
 
 export function addDependencies(options: SchemaOptions): Rule {
   return (tree: Tree, context: SchematicContext) => {
-    console.log('options', options, tree, context);
+    if (options.enforceConventionalCommits) {
+      addCommitizen(options, tree, context);
+    }
   };
 }
 
+export function addCommitizen(
+  options: SchemaOptions,
+  tree: Tree,
+  context: SchematicContext
+) {
+  addCommitizenDeps(options, tree, context);
+  addCommitizenConfig(tree);
+  addCommitizenScrip(tree);
+}
 
+export function addCommitizenDeps(
+  options: SchemaOptions,
+  tree: Tree,
+  context: SchematicContext
+) {
+  const packageJson: PackageJson = getPackageJson(tree);
+  const deps = {
+    commitizen: '^4.2.4',
+    'cz-conventional-changelog': '^3.3.0',
+  };
+  const devDependencies = {
+    devDependencies: { ...packageJson.devDependencies, ...deps },
+  };
+  const newJson: PackageJson = getMergedPackageJsonConfig(
+    devDependencies,
+    packageJson
+  ) as PackageJson;
+  overwritePackageJson(tree, newJson);
+  installDependencies(options, context);
+}
+
+export function addCommitizenConfig(tree: Tree) {
+  const packageJson: PackageJson = getPackageJson(tree);
+  const hasConfigFile = tree.exists('.czrc');
+
+  if (!packageJson?.config?.commitizen && !hasConfigFile) {
+    const config = {
+      config: {
+        ...packageJson?.config,
+        commitizen: {
+          path: 'cz-conventional-changelog',
+        },
+      },
+    };
+    const newJson: PackageJson = getMergedPackageJsonConfig(
+      config,
+      packageJson
+    ) as PackageJson;
+    overwritePackageJson(tree, newJson);
+  }
+}
+
+export function addCommitizenScrip(tree: Tree) {
+  const packageJson: PackageJson = getPackageJson(tree);
+  const scripts = {
+    scripts: { ...packageJson.scripts, ...{ cz: 'cz' } },
+  };
+  const newJson: PackageJson = getMergedPackageJsonConfig(
+    scripts,
+    packageJson
+  ) as PackageJson;
+  overwritePackageJson(tree, newJson);
+}
+
+function installDependencies(
+  options: SchemaOptions,
+  context: SchematicContext
+) {
+  if (options.skipInstall !== true) {
+    context.logger.info('Installs npm dependencies');
+    context.addTask(new NodePackageInstallTask());
+  }
+}
---
+++ packages/semver/src/schematics/ng-add/utils/package-json.ts
@@ -0,0 +1,71 @@
+import { SchematicsException, Tree } from '@angular-devkit/schematics';
+import { join } from 'path';
+
+const PACKAGE_JSON = 'package.json';
+
+export interface PackageJson {
+  scripts: PackageJsonConfigPart<string>;
+  devDependencies: PackageJsonConfigPart<string>;
+  config?: {
+    commitizen?: any;
+  };
+  [key: string]: any;
+}
+
+export interface PackageJsonConfigPart<T> {
+  [key: string]: T;
+}
+
+class FileNotFoundException extends Error {
+  constructor(fileName: string) {
+    const message = `File ${fileName} not found!`;
+    super(message);
+  }
+}
+
+export const getPackageJson = (
+  tree: Tree,
+  workingDirectory = ''
+): PackageJson => {
+  const url = join(workingDirectory, PACKAGE_JSON);
+
+  return getJsonFile(tree, url);
+};
+
+export function getJsonFile<T>(tree: Tree, path: string): T {
+  const file = tree.get(path);
+  if (!file) {
+    throw new FileNotFoundException(path);
+  }
+
+  try {
+    const content = JSON.parse(file.content.toString());
+
+    return content as T;
+  } catch (e) {
+    throw new SchematicsException(`File ${path} could not be parsed!`);
+  }
+}
+
+export function getFileContents(tree: Tree, filePath: string): string {
+  const buffer = tree.read(filePath) || '';
+
+  return buffer.toString();
+}
+
+export function overwritePackageJson(
+  tree: Tree,
+  content: PackageJson,
+  workingDirectory = ''
+) {
+  const url = join(workingDirectory, PACKAGE_JSON);
+
+  tree.overwrite(url, JSON.stringify(content, null, 2));
+}
+
+export function getMergedPackageJsonConfig(
+  existingConfig: PackageJsonConfigPart<any>,
+  newConfig: PackageJsonConfigPart<any>
+) {
+  return { ...newConfig, ...existingConfig };
+}
