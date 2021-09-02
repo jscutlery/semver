7d9e4c9a2e50cbe4dbd77fe7e6a77ba2a2946351
travis.jones <travis.jones@cdinteractive.com>
2021-09-02 13:19:55 -0400

    feat(semver): Adding in a `--use-deps` flag to include dependencies when calculating the version bump

---
+++ packages/semver/src/executors/version/utils/get-greatest-version-bump.spec.ts
@@ -0,0 +1,31 @@
+import { getGreatestVersionBump } from './get-greatest-version-bump';
+
+describe('getGreatestVersionBump', () => {
+  // This should only be used when we're calculating a version bump, which should only be release versions.
+  describe('release versions', () => {
+    it('returns a value when only one value is supplied in the array', () => {
+      const version = getGreatestVersionBump(['0.0.1']);
+      expect(version).toEqual('0.0.1');
+    });
+
+    it('returns the greatest value when the greatest is in the front of the list', () => {
+      const version = getGreatestVersionBump(['1.0.0', '0.0.1', '0.1.0']);
+      expect(version).toEqual('1.0.0');
+    });
+
+    it('returns the greatest value when the greatest is in the middle of the list', () => {
+      const version = getGreatestVersionBump(['0.0.1', '0.1.0', '1.0.0', '0.0.1', '0.1.0']);
+      expect(version).toEqual('1.0.0');
+    });
+
+    it('returns the greatest value when the greatest is at the end of the list', () => {
+      const version = getGreatestVersionBump(['0.0.1', '0.0.1', '0.0.1', '0.0.1', '0.1.0']);
+      expect(version).toEqual('0.1.0');
+    });
+
+    it('returns a value when there\'s no winner', () => {
+      const version = getGreatestVersionBump(['0.0.1', '0.0.1', '0.0.1', '0.0.1']);
+      expect(version).toEqual('0.0.1');
+    });
+  })
+});
