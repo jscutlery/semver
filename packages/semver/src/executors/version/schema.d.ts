94fbed99880afe3d73b4a2ce4b5fe24637902480
Edouard Bozon <bozonedouard@gmail.com>
2021-09-28 22:25:56 +0200

    wip(semver): add post targets

--- packages/semver/src/executors/version/schema.d.ts
+++ packages/semver/src/executors/version/schema.d.ts
@@ -1,8 +1,20 @@
+import { TargetConfiguration } from "@nrwl/devkit";
+
 export type ReleaseIdentifier = 'patch' | 'minor' | 'major' | 'premajor' | 'preminor' | 'prepatch' | 'prerelease';
+
+/**
+ * Specify a target to run after a new version was successfully created.
+ * @example "@jscutlery/semver:push" | { "executor": "@jscutlery/semver:push" }
+ */
+export type PostTargetSchema = string | TargetConfiguration;
 
 export interface VersionBuilderSchema {
   dryRun?: boolean;
   noVerify?: boolean;
+  /**
+   * @deprecated Use postTargets instead.
+   * @sunset 4.0.0
+   */
   push?: boolean;
   remote?: string;
   baseBranch?: string;
@@ -18,4 +30,5 @@ export interface VersionBuilderSchema {
   preid?: string;
   changelogHeader?: string;
   versionTagPrefix?: string;
+  postTargets: PostTargetSchema[];
 }
