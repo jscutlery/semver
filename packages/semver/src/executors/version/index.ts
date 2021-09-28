199ac0a5692bc929b9c452743076eafc40fe5eee
Edouard Bozon <bozonedouard@gmail.com>
2021-09-28 22:25:56 +0200

    wip(semver): add post targets

--- packages/semver/src/executors/version/index.ts
+++ packages/semver/src/executors/version/index.ts
@@ -1,4 +1,4 @@
-import { ExecutorContext, logger } from '@nrwl/devkit';
+import { logger, parseTargetString, runExecutor } from '@nrwl/devkit';
 import { concat, defer, of } from 'rxjs';
 import { catchError, mapTo, switchMap } from 'rxjs/operators';
 
@@ -8,8 +8,22 @@ import { tryBump } from './utils/try-bump';
 import { getProjectRoot } from './utils/workspace';
 import { versionProject, versionWorkspace } from './version';
 
+import type { ExecutorContext } from '@nrwl/devkit';
 import type { CommonVersionOptions } from './version';
-import type { VersionBuilderSchema } from './schema';
+import type { PostTargetSchema, VersionBuilderSchema } from './schema';
+
+export function normalizePostTarget(
+  postTargetSchema: PostTargetSchema,
+  context: ExecutorContext
+): Parameters<typeof runExecutor> {
+  return typeof postTargetSchema === 'string'
+    ? [parseTargetString(postTargetSchema), {}, context]
+    : [
+        parseTargetString(postTargetSchema.executor),
+        postTargetSchema.options,
+        context,
+      ];
+}
 
 export default function version(
   {
@@ -26,17 +40,23 @@ export default function version(
     preid,
     changelogHeader,
     versionTagPrefix,
+    postTargets,
   }: VersionBuilderSchema,
   context: ExecutorContext
 ): Promise<{ success: boolean }> {
   const workspaceRoot = context.root;
   const preset = 'angular';
-  const tagPrefix = versionTagPrefix !== undefined
-    ? resolveTagTemplate(
-      versionTagPrefix,
-      { target: context.projectName, projectName: context.projectName }
-    )
-    : (syncVersions ? 'v' : `${context.projectName}-`);
+
+  // @todo: refactor this in a dedicated function without a chained ternary.
+  const tagPrefix =
+    versionTagPrefix !== undefined
+      ? resolveTagTemplate(versionTagPrefix, {
+          target: context.projectName,
+          projectName: context.projectName,
+        })
+      : syncVersions
+      ? 'v'
+      : `${context.projectName}-`;
 
   const projectRoot = getProjectRoot(context);
   const newVersion$ = tryBump({
@@ -74,6 +94,8 @@ export default function version(
             })
           : versionProject(options)
       );
+
+      // @todo 4.0.0: remove this in favor of @jscutlery/semver:push postTarget.
       const pushToGitRemote$ = defer(() =>
         tryPushToGitRemote({
           branch: baseBranch,
@@ -82,18 +104,33 @@ export default function version(
         })
       );
 
+      const executePostTargets = postTargets.map((postTarget) => {
+        const options = normalizePostTarget(postTarget, context);
+        return defer(async () => {
+          const run = await runExecutor(...options);
+          for await (const result of run) {
+            if (!result.success) {
+              throw new Error(`Something went wrong with post target: "${options[0].project}:${options[0].target}"`)
+            }
+          }
+        });
+      });
+
       return concat(
         runStandardVersion$,
-        ...(push && dryRun === false ? [pushToGitRemote$] : [])
+        ...(push && dryRun === false ? [pushToGitRemote$] : []),
+        ...(dryRun === false ? executePostTargets : [])
       );
     })
   );
 
-  return action$.pipe(
-    mapTo({ success: true }),
-    catchError((error) => {
-      logger.error(error.stack ?? error.toString());
-      return of({ success: false });
-    })
-  ).toPromise();
+  return action$
+    .pipe(
+      mapTo({ success: true }),
+      catchError((error) => {
+        logger.error(error.stack ?? error.toString());
+        return of({ success: false });
+      })
+    )
+    .toPromise();
 }
