08b90d6c1a9957ef57bd36f557e9252e2b281d6a
Edouard Bozon <bozonedouard@gmail.com>
2021-10-06 13:21:30 +0200

    chore: 🛠 customize commitlint config

---
+++ commitlint.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  extends: ['@commitlint/config-angular'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'revert',
+        'test',
+        'wip',
+        'chore',
+      ],
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      ['semver']
+    ]
+  },
+};
