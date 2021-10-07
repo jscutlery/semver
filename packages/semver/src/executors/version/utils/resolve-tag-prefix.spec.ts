4cc714816e136ae5e3f87fb4edaf92ca259e2f97
RaviTejaVattem <ravitejavattem@gmail.com>
2021-10-07 18:37:15 +0530

    refactor(semver): refactored tag prefix value to a pure function

---
+++ packages/semver/src/executors/version/utils/resolve-tag-prefix.spec.ts
@@ -0,0 +1,33 @@
+import { resolveTagPrefix } from './resolve-tag-prefix';
+
+describe(resolveTagPrefix.name, () => {
+  it('should resolve interpolated string', () => {
+    expect(
+      resolveTagPrefix({
+        versionTagPrefix: 'testVersionTagPrefix',
+        projectName: 'testProjectName',
+        syncVersions: true,
+      })
+    ).toBe('testVersionTagPrefix');
+  });
+
+  it('should resolve syncVersions', () => {
+    expect(
+      resolveTagPrefix({
+        versionTagPrefix: undefined,
+        projectName: 'testProjectName',
+        syncVersions: true,
+      })
+    ).toBe('v');
+  });
+
+  it('should resolve default tag', () => {
+    expect(
+      resolveTagPrefix({
+        versionTagPrefix: undefined,
+        projectName: 'testProjectName',
+        syncVersions: false,
+      })
+    ).toBe('testProjectName-');
+  });
+});
