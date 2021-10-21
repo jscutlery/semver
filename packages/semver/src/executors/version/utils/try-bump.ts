15f4c3cadbd0e35569b5c2bd7abc7d77e6d4d753
travis.jones <travis.jones@cdinteractive.com>
2021-10-21 11:43:15 -0400

    fix: cleaning up after a rebase

--- packages/semver/src/executors/version/utils/try-bump.ts
+++ packages/semver/src/executors/version/utils/try-bump.ts
@@ -87,7 +87,7 @@ If your project is already versioned, please tag the latest release commit with 
       const numOfCommits = commits.reduce((acc, dep) => acc + dep.length, 0);
       /* No commits since last release so don't bump. */
       if (numOfCommits === 0) {
-        return of(null);
+        return of(undefined);
       }
 
       const dependencyChecks$ = dependencyRoots.map((root) =>
