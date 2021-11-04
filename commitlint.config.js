2b085ebc674f3ac54dfb9f9972b7b3fe4742a615
Edouard Bozon <bozonedouard@gmail.com>
2021-11-04 14:42:02 +0100

    ci: stick to `@commitlint/config-angular` types

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,22 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-angular'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      [
-        'ci',
-        'docs',
-        'feat',
-        'fix',
-        'perf',
-        'refactor',
-        'revert',
-        'test',
-        'wip',
-        'chore',
-      ],
-    ],
     'scope-enum': [
       2,
       'always',
