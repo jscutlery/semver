4cd0d7008b2acb2b9c8beb56bb9795f7919ff6f0
Edouard Bozon <bozonedouard@gmail.com>
2022-02-24 10:19:37 +0100

    fix(semver): 🐞 bump `@commitlint/*` deps

--- packages/semver/src/generators/install/utils/dependencies.ts
+++ packages/semver/src/generators/install/utils/dependencies.ts
@@ -32,8 +32,8 @@ function _addDevDependencies(tree: Tree, options: SchemaOptions) {
       tree,
       {},
       {
-        '@commitlint/cli': '^16.0.1',
-        '@commitlint/config-angular': '^16.0.0',
+        '@commitlint/cli': '^16.2.1',
+        '@commitlint/config-angular': '^16.2.1',
         husky: '^7.0.4',
       }
     );
