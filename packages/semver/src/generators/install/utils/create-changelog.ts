3e482e65818b201658aed19db60008315f99a27c
Michael Berger <m.gonser@icloud.com>
2023-06-26 08:02:30 +0200

    refactor: rename addChangelog to createChangelog to follow naming conv

--- packages/semver/src/generators/install/utils/create-changelog.ts
+++ packages/semver/src/generators/install/utils/create-changelog.ts
@@ -1,6 +1,9 @@
 import { generateFiles, joinPathFragments, Tree } from '@nx/devkit';
 
-export function addChangelog(tree: Tree, libraryRoot: string) {
+export function createChangelog(tree: Tree, libraryRoot: string) {
+  if (tree.exists(joinPathFragments(libraryRoot, 'CHANGELOG.md'))) {
+    return;
+  }
   generateFiles(
     tree,
     joinPathFragments(__dirname, '../__files'), // path to the file templates
