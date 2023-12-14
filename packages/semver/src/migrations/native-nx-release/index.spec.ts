d852ac4f800d0cbe1a85d7b7ba1a2e1f06e8ec21
Edouard Bozon <bozonedouard@gmail.com>
2023-12-14 16:24:31 +0100

    wip: add post targets removal

--- packages/semver/src/migrations/native-nx-release/index.spec.ts
+++ packages/semver/src/migrations/native-nx-release/index.spec.ts
@@ -9,7 +9,9 @@ describe('native nx release migration', () => {
 
   beforeEach(async () => {
     tree = createTreeWithEmptyWorkspace();
+  });
 
+  it('should remove version target', () => {
     addProjectConfiguration(tree, 'a', {
       root: 'libs/a',
       targets: {
@@ -20,12 +22,54 @@ describe('native nx release migration', () => {
     });
 
     migrate(tree);
+
+    const projectConfig = getProjects(tree).get('a');
+    expect(projectConfig?.targets?.version).toBeUndefined();
   });
 
-  it('should remove version target', () => {
+  it('should remove post targets', () => {
+    addProjectConfiguration(tree, 'a', {
+      root: 'libs/a',
+      targets: {
+        version: {
+          executor: '@jscutlery/semver:version',
+          options: {
+            postTargets: ['npm', 'github'],
+          },
+        },
+        npm: {
+          command: 'exit 0',
+        },
+        github: {
+          command: 'exit 0',
+        },
+      },
+    });
+
+    migrate(tree);
+
     const projectConfig = getProjects(tree).get('a');
-    expect(projectConfig).toBeDefined();
-    expect(projectConfig?.targets?.version).toBeUndefined();
+    expect(projectConfig?.targets?.npm).toBeUndefined();
+    expect(projectConfig?.targets?.github).toBeUndefined();
+  });
+
+  it('should bail out if sync mode is detected', () => {
+    addProjectConfiguration(tree, 'a', {
+      root: '.',
+      targets: {
+        version: {
+          executor: '@jscutlery/semver:version',
+          options: {
+            syncVersions: true,
+          },
+        },
+      },
+    });
+
+    migrate(tree);
+
+    const projectConfig = getProjects(tree).get('a');
+    expect(projectConfig?.targets?.version).toBeDefined();
   });
 
   it.todo('should remove @jscutlery/semver from package.json');
--- packages/semver/src/migrations/native-nx-release/index.ts
+++ packages/semver/src/migrations/native-nx-release/index.ts
@@ -1,16 +1,54 @@
-import { getProjects, updateProjectConfiguration, type Tree, logger } from '@nx/devkit';
+import {
+  getProjects,
+  updateProjectConfiguration,
+  type Tree,
+  logger,
+} from '@nx/devkit';
 
 export default function migrate(tree: Tree) {
-  Array.from(getProjects(tree)).filter(([, projectConfig]) => {
-    return projectConfig.targets?.version?.executor === '@jscutlery/semver:version'
-  }).forEach(([projectName, projectConfig]) => {
-    logger.info(`Updating project "${projectName}", removing version target.`);
+  const projects = Array.from(getProjects(tree));
+  const syncModeDetected = projects.some(([, projectConfig]) => {
+    return projectConfig.targets?.version?.options?.syncVersions === true;
+  });
+
+  if (syncModeDetected) {
+    logger.info(
+      'Sync mode detected, skipping migration. Please migrate your workspace manually.',
+    );
+    return;
+  }
+
+  const semverProjects = projects.filter(([, projectConfig]) => {
+    return (
+      projectConfig.targets?.version?.executor === '@jscutlery/semver:version'
+    );
+  });
+
+  semverProjects.forEach(([projectName, projectConfig]) => {
+    logger.info(
+      `Updating project "${projectName}", removing "version" target.`,
+    );
     // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-non-null-assertion
     const { version, ...targets } = projectConfig.targets!;
+    const postTargets = (version?.options?.postTargets as string[]) ?? [];
+
+    if (postTargets.length > 0) {
+      logger.info(
+        `Updating project "${projectName}", removing post targets: "${postTargets.join(
+          '", "',
+        )}"`,
+      );
+    }
+
+    postTargets.forEach((postTarget) => {
+      delete targets[postTarget];
+    });
+
     const newProjectConfig = {
       ...projectConfig,
       targets,
     };
+
     updateProjectConfiguration(tree, projectName, newProjectConfig);
   });
 }
